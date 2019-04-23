7c1c63ac075a6e4f714286c8d294824d8e9370a8
thomas9911 <thomastimmer11@hotmail.com>
2019-04-23 22:27:11 +0200

    travis add windows

--- .deploy.sh
+++ .deploy.sh
@@ -8,17 +8,29 @@ echo "$RUST_TOOLCHAIN"
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     FILE=/home/travis/build/thomas9911/openstack-client/target/release/openstack-client
     CLIENT_VERSION=$($FILE --version | sed 's/openstack-client//' | tr -d '[:space:]')
-    ls
-    pwd
     echo "$RUST_VERSION"
     echo "$RUST_TOOLCHAIN"
-    echo $FILE
     echo "$CLIENT_VERSION"
     strip "$FILE"
     $FILE upload object --file "$FILE" --name "$CLIENT_VERSION"/"$TRAVIS_OS_NAME"/"$RUST_TOOLCHAIN"/"$RUST_VERSION"/openstack-client --container rustci
 fi
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    FILE=/Users/travis/build/thomas9911/openstack-client/target/release/openstack-client
+    CLIENT_VERSION=$($FILE --version | sed 's/openstack-client//' | tr -d '[:space:]')
+    echo "$RUST_VERSION"
+    echo "$RUST_TOOLCHAIN"
+    strip "$FILE"
+    $FILE upload object --file "$FILE" --name "$CLIENT_VERSION"/"$TRAVIS_OS_NAME"/"$RUST_TOOLCHAIN"/"$RUST_VERSION"/openstack-client --container rustci
+fi
+
+if [ "$TRAVIS_OS_NAME" == "windows" ]; then
+    pwd
+    ls
+    sed --version
+    tr --version
+    strip --version
+
     FILE=/Users/travis/build/thomas9911/openstack-client/target/release/openstack-client
     CLIENT_VERSION=$($FILE --version | sed 's/openstack-client//' | tr -d '[:space:]')
     echo "$RUST_VERSION"
