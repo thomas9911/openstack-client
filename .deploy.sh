6f1c67ea70bbb4362078c1039e2c13a8a20614c9
thomas9911 <thomastimmer11@hotmail.com>
2019-04-23 21:02:14 +0200

    travis add script

---
+++ .deploy.sh
@@ -0,0 +1,24 @@
+RUST_VERSION=$(rustc --version | sed 's/rustc //' | sed 's/(.*)//' | tr -d [:space:])
+RUST_TOOLCHAIN=$(rustup show active-toolchain | sed 's/(default)//' | tr -d [:space:])
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    FILE=/home/travis/build/thomas9911/openstack-client/target/release/openstack-client
+    CLIENT_VERSION=$($FILE --version | sed 's/openstack-client//' | tr -d [:space:])
+    ls
+    pwd
+    echo $RUST_VERSION
+    echo $RUST_TOOLCHAIN
+    echo $FILE
+    echo $CLIENT_VERSION
+    strip "$FILE"
+    $FILE upload object --file "$FILE" --name $CLIENT_VERSION/$TRAVIS_OS_NAME/$RUST_TOOLCHAIN/$RUST_VERSION/openstack-client --container rustci
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    ls
+    pwd
+    echo $RUST_VERSION
+    echo $RUST_TOOLCHAIN
+    # strip /home/travis/build/thomas9911/openstack-client/target/release/openstack-client
+    # /home/travis/build/thomas9911/openstack-client/target/release/openstack-client upload object --file /home/travis/build/thomas9911/openstack-client/target/release/openstack-client --name $CLIENT_VERSION/$TRAVIS_OS_NAME/$RUST_TOOLCHAIN/$RUST_VERSION/openstack-client --container rustci
+fi
